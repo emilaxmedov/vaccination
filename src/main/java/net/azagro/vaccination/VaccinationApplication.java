bd38ac750d6657d8049d352ef985014bd0e9d13b
Emil Axmedov <emil.axmedov@gmail.com>
2017-12-11 18:03:12 +0400

    First commit

---
+++ src/main/java/net/azagro/vaccination/VaccinationApplication.java
@@ -0,0 +1,12 @@
+package net.azagro.vaccination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinationApplication.class, args);
+	}
+}
---
+++ src/test/java/net/azagro/vaccination/VaccinationApplicationTests.java
@@ -0,0 +1,16 @@
+package net.azagro.vaccination;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VaccinationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
